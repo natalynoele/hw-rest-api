098634c3d4c78ba62c99ffca47a31b9fede8191c
natalynoele <natalynoele@gmail.com>
2023-06-09 22:42:57 +0300

    REFACTOR: included the UserController class instead of the files;  delete register and login files

--- controllers/users/login.js
+++
@@ -1,8 +0,0 @@
-const { User } = require("../../models");
-
-const login = async (req, res) => {
-  const result = await User.create(req.body);
-  return res.status(200).json(result);
-};
-
-module.exports = login;
